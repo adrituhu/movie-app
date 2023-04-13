ff8a9c476626f72668e318e1bf2d6516c67b03fa
sofyan ardiansyah <sofyanardianysah@gmail.com>
2023-04-13 22:28:54 +0800

    add vite url

--- src/api.js
+++ src/api.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
-// const apiKey = import.meta.env.REACT_APP_APIKEY;
-// const baseUrl = import.meta.env.REACT_APP_BASEURL;
-
+const apiKey = import.meta.env.VITE_APIKEY;
+const baseUrl = import.meta.env.VITE_BASEURL;
+console.log(apiKey)
+console.log(baseUrl)
 export const getMovieList = async () => {
   const movie = await axios.get(`${baseUrl}/movie/popular?api_key=${apiKey}`);
   console.log(apiKey);
